1e3d9100b5a155cabe308b6e92c777404ea12dd0
admin <harshkurulkar7@gmail.com>
2024-04-11 09:54:59 +0530

    Problem 1 completed

---
+++ SpringHandsOn/src/main/java/com/learning/spring/day1/D05P02.java
@@ -0,0 +1,16 @@
+package com.learning.spring.day1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class D05P02 {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+	
+		Battery bat= (Battery)context.getBean("battery");
+		System.out.println(bat);
+		
+		Disc disc= (Disc)context.getBean("disc");
+		System.out.println(disc);
+	}
+}
